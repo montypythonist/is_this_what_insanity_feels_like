9da9a7cc231de0105a3cec52e9527078a500862a
montypythonist <julia.alotoom.contact@gmail.com>
2025-10-03 12:39:54 -0700

    calc is short for calculator chat

---
+++ java-calc/calc.java
@@ -0,0 +1,17 @@
+// calc is short for calculator by the way chat, its slang
+import java.util.*;
+public class calc{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("input a number: ");
+        int num1 = input.nextInt();  
+        System.out.print("do you want to add, subtract, multiply, or divide? (input the symbol by itself: +, -, x, /): ");
+        String symbol = input.nextLine();
+        System.out.print("input a SECOND number... heh...: ");
+        int num2 = input.nextInt(); 
+        if (symbol == "+") {
+            System.out.println()
+
+        }
+    }
+}
